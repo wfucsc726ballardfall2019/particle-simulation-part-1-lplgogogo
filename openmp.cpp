6d3be7307c70c43275239f8d4aba65668650964a
fredzhangziji <52152169+fredzhangziji@users.noreply.github.com>
2019-10-25 21:53:14 -0400

    Update openmp.cpp
    #1 Added input for number of threads
    #2 Parallelized the biggest for loop (but couldn't parallelize with collapse, and maybe the reason is that we have if condition outside the inner for loop)
    #3 Tried to parallelized the two for loops of "move particles" and "resize bin vector", but the simulated time increased, so those pragmas are commented out.

--- openmp.cpp
+++ openmp.cpp
@@ -3,96 +3,223 @@
 #include <assert.h>
 #include <math.h>
 #include "common.h"
+#include <vector>
+#include <iostream>
 #include "omp.h"
+using namespace std;
+
+// constant copied from common.cpp for use
+#define density 0.0005
+#define mass    0.01
+#define cutoff  0.01
+#define min_r   (cutoff/100)
+#define dt      0.0005
 
 //
 //  benchmarking program
 //
 int main( int argc, char **argv )
-{   
-    int navg,nabsavg=0,numthreads; 
-    double dmin, absmin=1.0,davg,absavg=0.0;
-	
+{    
+    int navg,nabsavg=0;
+    double davg,dmin, absmin=1.0, absavg=0.0;
+
     if( find_option( argc, argv, "-h" ) >= 0 )
     {
         printf( "Options:\n" );
         printf( "-h to see this help\n" );
-        printf( "-n <int> to set number of particles\n" );
+        printf( "-n <int> to set the number of particles\n" );
         printf( "-o <filename> to specify the output file name\n" );
-        printf( "-s <filename> to specify a summary file name\n" ); 
-        printf( "-no turns off all correctness checks and particle output\n");   
+        printf( "-s <filename> to specify a summary file name\n" );
+        printf( "-no turns off all correctness checks and particle output\n");
         return 0;
     }
-
+    
     int n = read_int( argc, argv, "-n", 1000 );
+    int thread = read_int(argc, argv, "-t", 1);
     char *savename = read_string( argc, argv, "-o", NULL );
     char *sumname = read_string( argc, argv, "-s", NULL );
 
+    omp_set_num_threads(thread);
+    
     FILE *fsave = savename ? fopen( savename, "w" ) : NULL;
-    FILE *fsum = sumname ? fopen ( sumname, "a" ) : NULL;      
+    FILE *fsum = sumname ? fopen ( sumname, "a" ) : NULL;
 
     particle_t *particles = (particle_t*) malloc( n * sizeof(particle_t) );
     set_size( n );
     init_particles( n, particles );
+    
+    //calculate the gridSize, binSize, and then number of bin on one side;
+    double gridSize = sqrt(n * density);
+    double binSize = cutoff * 2;     // equals to the diameter of the circle
+    int binNum = int(gridSize / binSize) + 1; // the binNum should be +1
+
+    vector<vector<int> >bin(binNum * binNum, vector<int> (0));
 
     //
     //  simulate a number of time steps
     //
     double simulation_time = read_timer( );
-
-    #pragma omp parallel private(dmin) 
-    {
-    numthreads = omp_get_num_threads();
+	
     for( int step = 0; step < NSTEPS; step++ )
     {
-        navg = 0;
+	    navg = 0;
         davg = 0.0;
-	dmin = 1.0;
-        //
-        //  compute all forces
-        //
-        #pragma omp for reduction (+:navg) reduction(+:davg)
-        for( int i = 0; i < n; i++ )
-        {
-            particles[i].ax = particles[i].ay = 0;
-            for (int j = 0; j < n; j++ )
-                apply_force( particles[i], particles[j],&dmin,&davg,&navg);
+	    dmin = 1.0;
+
+        //put all the particles into corresponding bins
+        //#pragma omp parallel for 
+        for (int i = 0; i < n; i++){
+            int row = floor(particles[i].x / binSize);     //calculate the row index of the bin
+            int col = floor(particles[i].y / binSize);     //calculate the column index of the bin
+            //#pragma omp critical(binPush)
+            bin[row * binNum + col].push_back(i);      //put the particle in to the bin in row major
         }
-        
-		
+
+        #pragma omp parallel for
+        for (int i = 0; i < n; i++){
+            particles[i].ax = particles[i].ay = 0;      // initialize acceleration
+            int row = floor(particles[i].x / binSize);     //calculate the row index of the bin
+            int col = floor(particles[i].y / binSize);     //calculate the column index of the bin
+
+            //
+            //different cases to deal with
+            //
+            //situation that the particle is not in the first or the last column of the grid
+            if ((row > 0) && (row< binNum-1)){
+                //#pragma omp parallel for
+                for (int j = row-1; j <= row+1; j++){
+                    //situation that the particle is not in the first or the last row of the grid
+                    if ((col > 0 )&&(col < binNum-1)){
+                        for (int k = col-1; k <= col+1; k++){
+                             for (int l = 0; l < bin[j*binNum + k].size(); l++){
+                                 int fa = bin[j*binNum + k].at(l);
+                                 apply_force(particles[i], particles[fa], &dmin, &davg, &navg);
+                            }
+                        }
+                    }
+
+                    //situation that the particle is in the first column of the grid
+                    else if (col == 0){
+                        for (int k = col; k <= col+1; k++){
+                            for (int l = 0; l < bin[j*binNum + k].size(); l++){
+                                 int fa = bin[j*binNum + k].at(l);
+                                 apply_force(particles[i], particles[fa], &dmin, &davg, &navg);
+                            }       
+                        }
+                    }
+                    //situation that the particle is in the last column of the grid
+                    else{
+                        for (int k = col-1; k <= col; k++){
+                            for (int l = 0; l < bin[j*binNum + k].size(); l++){
+                                 int fa = bin[j*binNum + k].at(l);
+                                 apply_force(particles[i], particles[fa], &dmin, &davg, &navg);                                 
+                            }
+                        }
+                    }
+                }
+            }
+
+            //situation that the particle is in the first row of the grid
+            else if (row == 0){
+                for (int j = row; j <= row+1; j++){
+                    //situation that the particle is not in the first or the last column of the grid
+                    if ((col > 0 )&& (col < binNum-1)){
+                        for (int k = col-1; k <= col+1; k++){
+                            for (int l = 0; l < bin[j*binNum + k].size(); l++){
+                                 int fa = bin[j*binNum + k].at(l);
+                                 apply_force(particles[i], particles[fa], &dmin, &davg, &navg);                                  
+                            }
+                        }
+                    }
+                    //situation that the particle is in the first column of the grid
+                    else if (col == 0){
+                        for (int k = col; k <= col+1; k++){
+                            for (int l = 0; l < bin[j*binNum + k].size(); l++){
+                                 int fa = bin[j*binNum + k].at(l);
+                                 apply_force(particles[i], particles[fa], &dmin, &davg, &navg);                                 
+                            }
+                        }
+                    }
+                    //situation that the particle is in the last column of the grid
+                    else if (col == binNum-1){
+                        for (int k = col-1; k <= col; k++){
+                            for (int l = 0; l <bin[j*binNum + k].size(); l++){
+                                 int fa = bin[j*binNum + k].at(l);
+                                 apply_force(particles[i], particles[fa], &dmin, &davg, &navg);                                 
+                            }
+                        }
+                    }
+                }
+            }
+
+            //situation that the particle is in the last row of the grid
+            if (row == binNum-1){
+                for (int j = row-1; j <= row; j++){
+                    //situation that the particle is not in the first or the last column of the grid
+                    if ((col > 0) &&( col < binNum-1)){
+                        for (int k = col-1; k <= col+1; k++){
+                            for (int l = 0; l < bin[j*binNum + k].size(); l++){
+                                 int fa = bin[j*binNum + k].at(l);
+                                 apply_force(particles[i], particles[fa], &dmin, &davg, &navg);                                  
+                            }
+                        }
+                    }
+                    //situation that the particle is in the first column of the grid
+                    else if (col == 0){
+                        for (int k = col; k <= col+1; k++){
+                            for (int l = 0; l < bin[j*binNum + k].size(); l++){
+                                 int fa = bin[j*binNum + k].at(l);
+                                 apply_force(particles[i], particles[fa], &dmin, &davg, &navg);                                 
+                            }
+                        }
+                    }
+                    //situation that the particle is in the last column of the grid
+                    else{
+                        for (int k = col-1; k <= col; k++){
+                            for (int l = 0; l < bin[j*binNum + k].size(); l++){
+                                 int fa = bin[j*binNum + k].at(l);
+                                 apply_force(particles[i], particles[fa], &dmin, &davg, &navg);                                  
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        //
+        //before moving the particles we need to release the inside vectors
+        //
+        //#pragma omp parallel for
+        for (int i = 0; i < binNum*binNum; i++)
+            bin[i].resize(0);
+
         //
         //  move particles
         //
-        #pragma omp for
+        //#pragma omp parallel for
         for( int i = 0; i < n; i++ ) 
-            move( particles[i] );
-  
-        if( find_option( argc, argv, "-no" ) == -1 ) 
+            move( particles[i] );		
+
+        if( find_option( argc, argv, "-no" ) == -1 )
         {
           //
-          //  compute statistical data
+          // Computing statistical data
           //
-          #pragma omp master
-          if (navg) { 
-            absavg += davg/navg;
+          if (navg) {
+            absavg +=  davg/navg;
             nabsavg++;
           }
-
-          #pragma omp critical
-	  if (dmin < absmin) absmin = dmin; 
+          if (dmin < absmin) absmin = dmin;
 		
           //
           //  save if necessary
           //
-          #pragma omp master
           if( fsave && (step%SAVEFREQ) == 0 )
               save( fsave, n, particles );
         }
     }
-}
     simulation_time = read_timer( ) - simulation_time;
     
-    printf( "n = %d,threads = %d, simulation time = %g seconds", n,numthreads, simulation_time);
+    printf( "n = %d, simulation time = %g seconds", n, simulation_time);
 
     if( find_option( argc, argv, "-no" ) == -1 )
     {
@@ -108,20 +235,19 @@ int main( int argc, char **argv )
     if (absmin < 0.4) printf ("\nThe minimum distance is below 0.4 meaning that some particle is not interacting");
     if (absavg < 0.8) printf ("\nThe average distance is below 0.8 meaning that most particles are not interacting");
     }
-    printf("\n");
-    
+    printf("\n");     
+
     //
     // Printing summary data
     //
-    if( fsum)
-        fprintf(fsum,"%d %d %g\n",n,numthreads,simulation_time);
-
+    if( fsum) 
+        fprintf(fsum,"%d %g\n",n,simulation_time);
+ 
     //
     // Clearing space
     //
     if( fsum )
-        fclose( fsum );
-
+        fclose( fsum );    
     free( particles );
     if( fsave )
         fclose( fsave );
